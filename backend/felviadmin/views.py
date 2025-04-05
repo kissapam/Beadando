3b265a0b2fd7cfccda1dec352373ba6ce65499a8
kissapam <kissrobilaci@gmail.com>
2025-04-05 07:10:17 +0200

    Negyedik verzió Képpel

--- backend/felviadmin/views.py
+++ backend/felviadmin/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+# kép beolvasásához fájl-ból kell importálni a következő sort
+from django.core.files.storage import FileSystemStorage
 
 from .models import Szak,Felvetelizo
 
-# Create your views here.
+# Create your views here
 def index(request):
     osszesAdat = Felvetelizo.objects.all().order_by("-szulEv")
     context = {"adatok":osszesAdat}
@@ -15,14 +17,27 @@ def addPage(request):   # az elsőben a renderelést oldom meg, kell a legördü
     return render(request,"add.html", context)
 
 # az ürlapból megkapja az adatokat és az adatbázisba rögzíti
-def addFelvetelizo(request):
-    if request.method == "POST":
+def addFelvetelizo(request): 
+    if request.method == "POST" and request.FILES["kep"]: # az end és ami ztánna van a képfeltöltéshez kell
         _szakId = request.POST["szak_valaszt"]
-        _szak = Szak.objects.get(pk=_szakId)
+        _szak = None # Az if-en belüli _szak változó nem lenne látható késöbb a felvételiző összeállításáshoz így előbb deklarálom
+        if _szakId == "addSzak": # ha a szakId-nak az addSzak értéket kaptam (mert az ürlapon új szakot adok hozzá) akkor
+            _ujSzak = request.POST["ujSzak"] # a kapott új szakot "ujSzak" fel kell vinni az adatbázisba
+            aktUjSzak = Szak(szakNev = _ujSzak, tamogatott = False) # itt állítom össze egy aktuális új szak objektumban
+            aktUjSzak.save() # itt mentem el a Szak modelben (táblában)
+            _szak = aktUjSzak # a felvételiző összeállításához pedig továbbítom az új szakot
+        else:
+            _szak = Szak.objects.get(pk=_szakId) # már meglévő szak lett kiválasztva, ez az id megy tovább a felvételiző összeállításához
         _nev = request.POST["nev"]
         _szulEv = request.POST["szulEv"]
         _pontszam = request.POST["pontszam"]
-        newFelvetelizo = Felvetelizo(szakId = _szak, nev = _nev, szulEv = _szulEv, pontszam = _pontszam)
+        # itt adom a képet az új felvételizőhöz
+        _kepAdatok = request.FILES["kep"]
+        fs = FileSystemStorage()
+        _kep = fs.save(_kepAdatok.name, _kepAdatok)
+
+        # és itt állítom össze az új példányt az osztályból
+        newFelvetelizo = Felvetelizo(szakId = _szak, nev = _nev, szulEv = _szulEv, pontszam = _pontszam, kep = _kep)
         newFelvetelizo.save()
     return redirect("/")    
 
